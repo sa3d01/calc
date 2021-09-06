19f64ee0e1cc6f3ccdd8d143d228a5ee69e734ba
sa3d01 <sa3dsalem01@gmail.com>
2021-09-06 19:06:32 +0200

    import

--- app/Imports/FormulanutrientsImport.php
+++ app/Imports/FormulanutrientsImport.php
@@ -4,7 +4,6 @@ namespace App\Imports;
 
 use App\Models\DropDown;
 use App\Models\FormulaNutrient;
-use App\Models\NutrientFactor;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
