62b73b64cfda801c62010fb36a3437ff4d1c8770
sa3d01 <sa3dsalem01@gmail.com>
2022-01-18 17:44:21 +0200

    stress factor

--- app/Http/Resources/DropDownCollection.php
+++ app/Http/Resources/DropDownCollection.php
@@ -19,8 +19,8 @@ class DropDownCollection extends ResourceCollection
             $arr['id'] = (int)$obj->id;
             $arr['name'] = $obj->name;
             if($obj->class=="ClinicalStatus"){
-                $arr['strees_factor_from'] = $obj->strees_factor_from;
-                $arr['strees_factor_to'] = $obj->strees_factor_to;
+                $arr['stress_factor_from'] = $obj->stress_factor_from;
+                $arr['stress_factor_to'] = $obj->stress_factor_to;
             }
             $data[] = $arr;
         }
